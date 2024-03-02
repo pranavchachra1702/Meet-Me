6a6f2899ea51684f69a699b4925feb4d8ab46d91
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2024-03-02 20:54:57 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const { Server } = require("socket.io");
+
+const io = new Server(3001, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log("Socket connected", socket.id);
+  socket.on("room:join", (data) => {
+    const { email, link ,name } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email);
+    io.to(link).emit("user:joined", { email, id: socket.id ,name });
+    socket.join(link);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer ,userName ,lastName }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer ,userName ,lastName });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ offer, to }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
